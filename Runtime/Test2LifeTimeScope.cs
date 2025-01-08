2209bc4af393103dfac2e5fbfc14d86668cb8541
sunsgogaming <sunsgo110@gmail.com>
2025-01-08 22:00:26 +0800

    test commit

---
+++ Runtime/Test2LifeTimeScope.cs
@@ -0,0 +1,24 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
+public class Test2LifeTimeScope : LifetimeScope
+{
+    protected override void Configure(IContainerBuilder builder)
+    {
+        base.Configure(builder);
+        
+        Debug.Log("Test2LifeTimeScope");
+
+        GameStart();
+    }
+
+    public async UniTask GameStart()
+    {
+        await UniTask.WaitForSeconds(1.0f);
+        Debug.Log("GameStart");
+        await UniTask.WaitForSeconds(1.0f);
+        Debug.Log("GameStart2");
+    }
+}
